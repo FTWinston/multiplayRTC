6403c75e9f24c047ab8ea063c13ce423dae49af6
FTWinston <ftwinston@gmail.com>
2021-08-31 22:40:29 +0100

    Correct build error in index.ts

--- src/framework/index.ts
+++ src/framework/index.ts
@@ -1,5 +1,5 @@
 export { ServerConnection } from './client/ServerConnection';
-export type { IServer } from './server/Server';
+export { IServer } from './server/IServer';
 export { IServerEntity, IServerRulesEntity } from './server/IServerEntity';
 export { LocalServerConnection } from './client/LocalServerConnection';
 export { RemoteServerConnection } from './client/RemoteServerConnection';
